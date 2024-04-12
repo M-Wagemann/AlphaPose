fba5ce49653d2e1cb63a76ef6306ddca99eca55a
Michel Wagemann <michel.wagemann@uni-bielefeld.de>
2024-04-12 13:12:30 +0200

    simpler solution to get the yolo models

--- alphapose/detector/yolo_cfg.py
+++ alphapose/detector/yolo_cfg.py
@@ -1,8 +1,8 @@
 from easydict import EasyDict as edict
 
 cfg = edict()
-cfg.CONFIG = 'configs/yolo/cfg/yolov3-spp.cfg'
-cfg.WEIGHTS = 'pretrained_models/yolo/yolov3-spp.weights'
+cfg.CONFIG = '/home/mwagemann/workspaces/lightpose/src/Alphapose/AlphaPose/configs/yolo/cfg/yolov3-spp.cfg'
+cfg.WEIGHTS = '/home/mwagemann/workspaces/lightpose/src/Alphapose/AlphaPose/pretrained_models/yolo/yolov3-spp.weights'
 cfg.INP_DIM =  608
 cfg.NMS_THRES =  0.6
 cfg.CONFIDENCE = 0.1
